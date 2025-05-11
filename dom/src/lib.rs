87d4207054ca1d49d0bdf11c5ee7b749417e9a27
tanagumo <nagumo5683@gmail.com>
2025-05-11 10:57:13 +0900

    chore: restructure project into Cargo workspace layout

---
+++ dom/src/lib.rs
@@ -0,0 +1,5 @@
+//! DOM tree construction module (placeholder)
+
+/// This crate will provide a structured representation of HTML based on the token stream.
+/// Currently a placeholder.
+pub fn not_implemented() {}
---
+++ query/src/lib.rs
@@ -0,0 +1,5 @@
+//! HTML query/select engine module (placeholder)
+
+/// This crate will provide CSS-style selectors and traversal APIs.
+/// Currently a placeholder.
+pub fn not_implemented() {}
